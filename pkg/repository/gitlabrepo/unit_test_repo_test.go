ecbbbe52eec5ecf70efc9ed3480b30360deaa00d
Julien PAPILLON <julien.papillon@gmail.com>
2024-01-06 14:47:56 +0100

    test: Complete unit testing fro Testing Repository

--- pkg/repository/gitlabrepo/unit_test_repo_test.go
+++ pkg/repository/gitlabrepo/unit_test_repo_test.go
@@ -1,6 +1,7 @@
 package gitlabrepo
 
 import (
+	"gitlab-service/pkg/openapi"
 	"gitlab-service/pkg/repository"
 	"os"
 	"testing"
@@ -53,7 +54,7 @@ func TestUnitTestRepoGetAllGroups(t *testing.T) {
 }
 
 func TestUnitTestRepoGetGroupSubgroups(t *testing.T) {
-	// Get for an unexisting group
+	// Get for a non-existent group
 	_, err := testRepo.GetGroupSubgroups(3)
 	if err == nil {
 		t.Error("no error when getting a non-existing group")
@@ -65,7 +66,7 @@ func TestUnitTestRepoGetGroupSubgroups(t *testing.T) {
 		t.Error("error when getting an empty group")
 	}
 	if len(groups) != 0 {
-		t.Errorf("getting subgroups for an empty groups: %d", len(groups))
+		t.Errorf("getting subgroups for an empty group: %d", len(groups))
 	}
 
 	// Get for a group with child group
@@ -75,5 +76,142 @@ func TestUnitTestRepoGetGroupSubgroups(t *testing.T) {
 	}
 	if len(groups) != 1 {
 		t.Errorf("this group should have exactly one subgroup: %d", len(groups))
+	}
+}
+
+func TestUnitTestRepoGetGroupProjects(t *testing.T) {
+	// Get for a non-existent group
+	_, err := testRepo.GetGroupProjects(3)
+	if err == nil {
+		t.Error("no error when getting a non-existing group")
+	}
+
+	// Get for a group without child project
+	projects, err := testRepo.GetGroupProjects(1)
+	if err != nil {
+		t.Error("error when getting an empty group")
+	}
+	if len(projects) != 0 {
+		t.Errorf("getting subgroups for an empty group: %d", len(projects))
+	}
+
+	// Get for a group with child projects
+	projects, err = testRepo.GetGroupProjects(2)
+	if err != nil {
+		t.Error("error when getting an non-empty group")
+	}
+	if len(projects) == 0 {
+		t.Error("this group should have projects")
+	}
+}
+
+func TestUnitTestRepoGetProjects(t *testing.T) {
+	// Get the groups
+	projects, err := testRepo.GetProjects()
+
+	// The error should always be returned nil
+	if err != nil {
+		t.Errorf("error is not nil: %v", err)
+	}
+
+	// We shoud have two groups
+	if len(projects) == 0 {
+		t.Errorf("no project")
+	}
+}
+
+func TestUnitRepoCreateProject(t *testing.T) {
+	// Try to create in an non-existent group
+	_, err := testRepo.CreateProject(&openapi.Project{
+		GroupId: int32adr(3),
+		Name:    stradr("Non existing group"),
+	})
+	if err == nil {
+		t.Error("no error when creating in a non-existing group")
+	}
+
+	// Get the number of projects
+	projects, _ := testRepo.GetProjects()
+	prevCount := len(projects)
+
+	// Create a project
+	id, err := testRepo.CreateProject(&openapi.Project{
+		Name:          stradr("New Project"),
+		GroupId:       int32adr(2),
+		DefaultBranch: stradr("main"),
+		HttpUrlToRepo: stradr("https://gitlab.example.com/foo/bar/new-project.git"),
+	})
+	if err != nil {
+		t.Errorf("error while creating a valid project: %v", err)
+	}
+
+	// Compare the number of projects
+	projects, _ = testRepo.GetProjects()
+	if len(projects) != prevCount+1 {
+		t.Errorf("invalid number of projects, shoud be %d but is %d", prevCount+1, len(projects))
+	}
+
+	// Check if the project exist
+	p, err := testRepo.GetProject(id)
+	if err != nil {
+		t.Errorf("error while getting newly created project: %v", err)
+	}
+	if *p.Name != "New Project" {
+		t.Errorf("project retrieved is not matching by name: %v", *p.Name)
+	}
+}
+
+func TestUnitRepoDeleteProject(t *testing.T) {
+	// Create a project
+	id, err := testRepo.CreateProject(&openapi.Project{
+		Name:          stradr("Project to be deleted"),
+		GroupId:       int32adr(2),
+		DefaultBranch: stradr("main"),
+		HttpUrlToRepo: stradr("https://gitlab.example.com/foo/bar/new-to-be-deleted.git"),
+	})
+	if err != nil {
+		t.Errorf("error while creating a valid project: %v", err)
+	}
+
+	// Get the number of projects
+	projects, _ := testRepo.GetProjects()
+	prevCount := len(projects)
+
+	// Delete the project
+	err = testRepo.DeleteProject(id)
+	if err != nil {
+		t.Errorf("error while deleting a valid project: %v", err)
+	}
+
+	// Compare the number of projects
+	projects, _ = testRepo.GetProjects()
+	if len(projects) != prevCount-1 {
+		t.Errorf("invalid number of projects, shoud be %d but is %d", prevCount-1, len(projects))
+	}
+
+	// Check the project does not exist
+	_, err = testRepo.GetProject(id)
+	if err == nil {
+		t.Errorf("no error while getting a deleted project")
+	}
+
+	// Try to delete a non-existing project
+	err = testRepo.DeleteProject(999)
+	if err == nil {
+		t.Error("no error when deleting a non-existing project")
+	}
+}
+
+func TestUnitRepoGetProject(t *testing.T) {
+	// Get for a non-existent project
+	_, err := testRepo.GetProject(999)
+	if err == nil {
+		t.Error("no error when getting a non-existing project")
+	}
+
+	// Get an existing project
+	_, err = testRepo.GetProject(1)
+	if err != nil {
+		t.Errorf("error while getting a valid project: %v", err)
 	}
 }
