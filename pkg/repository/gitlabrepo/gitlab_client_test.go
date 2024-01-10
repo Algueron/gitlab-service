26118d33e13f0e52f38e7a7fe47c35abde882df9
Julien PAPILLON <julien.papillon@gmail.com>
2024-01-10 19:59:41 +0100

    test: Add integration test for connecting to Gitlab

--- pkg/repository/gitlabrepo/gitlab_client_test.go
+++ pkg/repository/gitlabrepo/gitlab_client_test.go
@@ -1,1 +1,26 @@
 package gitlabrepo
+
+import "testing"
+
+func TestGitlabClientRepoConnect(t *testing.T) {
+	// Only during integration tests
+	if testing.Short() {
+		t.Skip("Skipping integration tests")
+	}
+
+	// Try to connect to invalid URL (empty)
+	emptyRepo := GitlabClientRepo{}
+	err := emptyRepo.Connect("://", "")
+	if err == nil {
+		t.Errorf("no error raised while connecting to an empty URL")
+	}
+
+	// Try to connect to a non-existing URL
+	wrongRepo := GitlabClientRepo{}
+	err = wrongRepo.Connect("http://test:60000", "test")
+	if err == nil {
+		t.Errorf("no error raised while connecting to a non-existing Repository")
+	}
+
+	// Successfull connection is already tested in TestMain func
+}
