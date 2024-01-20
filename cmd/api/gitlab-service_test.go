bec65c40ba1788ac56c8c4938cc128c5014b0931
Julien PAPILLON <julien.papillon@gmail.com>
2024-01-20 10:28:55 +0100

    test: Add testing for GetAllGroups handler

--- cmd/api/gitlab-service_test.go
+++ cmd/api/gitlab-service_test.go
@@ -1,1 +1,24 @@
 package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGitlabServiceGetAllGroups(t *testing.T) {
+	// Create the API request
+	req, _ := http.NewRequest("GET", "/groups", nil)
+
+	// Create a Recorder
+	rr := httptest.NewRecorder()
+
+	// Create a test server and call it
+	handler := http.HandlerFunc(app.GetAllGroups)
+	handler.ServeHTTP(rr, req)
+
+	// Check return status
+	if rr.Code != http.StatusOK {
+		t.Errorf("GET /groups: wront status returned; expected %d but got %d", http.StatusOK, rr.Code)
+	}
+}
