2c7002be037e8883b8d74ef6a27927d405b1edbc
Julien PAPILLON <julien.papillon@gmail.com>
2024-01-20 10:48:20 +0100

    test: Refactor handlers testing to implement table testing

--- cmd/api/gitlab-service_test.go
+++ cmd/api/gitlab-service_test.go
@@ -3,22 +3,51 @@ package api
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestGitlabServiceGetAllGroups(t *testing.T) {
-	// Create the API request
-	req, _ := http.NewRequest("GET", "/groups", nil)
+func TestGitlabServiceHandlers(t *testing.T) {
+	var tests = []struct {
+		name           string
+		method         string
+		url            string
+		jsonBody       string
+		handler        http.HandlerFunc
+		expectedStatus int
+	}{
+		{
+			name:           "GetAllGroups",
+			method:         "GET",
+			url:            "/groups",
+			jsonBody:       "",
+			handler:        app.GetAllGroups,
+			expectedStatus: http.StatusOK,
+		},
+	}
 
-	// Create a Recorder
-	rr := httptest.NewRecorder()
+	for _, test := range tests {
 
-	// Create a test server and call it
-	handler := http.HandlerFunc(app.GetAllGroups)
-	handler.ServeHTTP(rr, req)
+		// Create the API request
+		var req *http.Request
+		if test.jsonBody == "" {
+			req, _ = http.NewRequest(test.method, test.url, nil)
+		} else {
+			req, _ = http.NewRequest(test.method, test.url, strings.NewReader(test.jsonBody))
+		}
 
-	// Check return status
-	if rr.Code != http.StatusOK {
-		t.Errorf("GET /groups: wront status returned; expected %d but got %d", http.StatusOK, rr.Code)
+		// Create the recorder
+		rr := httptest.NewRecorder()
+
+		// Create the handler
+		handler := http.HandlerFunc(test.handler)
+
+		// Server the request
+		handler.ServeHTTP(rr, req)
+
+		// Check the returned code
+		if rr.Code != test.expectedStatus {
+			t.Errorf("%s: wrong status code returned; expected %d but got %d", test.name, test.expectedStatus, rr.Code)
+		}
 	}
 }
